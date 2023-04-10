7e98ce4108b9172ae0b633e310b311c72b7513d4
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-04-10 11:48:11 -0700

    Update follow-button.tsx

--- src/components/ui/follow-button.tsx
+++ src/components/ui/follow-button.tsx
@@ -41,7 +41,7 @@ export function FollowButton({
       >
         <ActionModal
           title={`Unfollow @${userTargetUsername}?`}
-          description='Their Tweets will no longer show up in your home timeline. You can still view their profile, unless their Tweets are protected.'
+          description='Their Claw will no longer show up in your home timeline. You can still view their profile, unless their Tweets are protected.'
           mainBtnLabel='Unfollow'
           action={handleUnfollow}
           closeModal={closeModal}
