096c2380200328d9d113497c1ecf134cf02edc45
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-04-10 11:56:36 -0700

    Update user-nav.tsx

--- src/components/user/user-nav.tsx
+++ src/components/user/user-nav.tsx
@@ -9,8 +9,8 @@ type UserNavProps = {
 
 const allNavs = [
   [
-    { name: 'Tweets', path: '' },
-    { name: 'Tweets & replies', path: 'with_replies' },
+    { name: 'Claws', path: '' },
+    { name: 'Claws & replies', path: 'with_replies' },
     { name: 'Media', path: 'media' },
     { name: 'Likes', path: 'likes' }
   ],
