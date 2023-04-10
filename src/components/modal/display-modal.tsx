461c7dc8d377687e71a8e6b8a9c30008cc465442
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-04-10 13:08:14 -0700

    Update display-modal.tsx

--- src/components/modal/display-modal.tsx
+++ src/components/modal/display-modal.tsx
@@ -38,7 +38,7 @@ export function DisplayModal({ closeModal }: DisplayModalProps): JSX.Element {
                    border-light-border px-4 py-3 dark:border-dark-border'
       >
         <div className='grid grid-cols-[auto,1fr] gap-3'>
-          <UserAvatar src='/assets/twitter-avatar.jpg' alt='Twitter' />
+          <UserAvatar src='/assets/twitter-avatar.png' alt='Twitter' />
           <div>
             <div className='flex gap-1'>
               <UserName verified name='Melo' />
@@ -95,3 +95,4 @@ export function DisplayModal({ closeModal }: DisplayModalProps): JSX.Element {
     </div>
   );
 }
+
