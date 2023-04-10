72da5f5f6fe0fe27fe3d6100634166ec345b3b87
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-04-10 12:52:47 -0700

    Update media.tsx

--- src/pages/user/[id]/media.tsx
+++ src/pages/user/[id]/media.tsx
@@ -33,7 +33,7 @@ export default function UserMedia(): JSX.Element {
   return (
     <section>
       <SEO
-        title={`Media Tweets by ${name as string} (@${
+        title={`Media Claws by ${name as string} (@${
           username as string
         }) / Twitter`}
       />
@@ -41,8 +41,8 @@ export default function UserMedia(): JSX.Element {
         <Loading className='mt-5' />
       ) : !sortedTweets ? (
         <StatsEmpty
-          title={`@${username as string} hasn't Tweeted Media`}
-          description='Once they do, those Tweets will show up here.'
+          title={`@${username as string} hasn't Clawed Media`}
+          description='Once they do, those Claws will show up here.'
           imageData={{ src: '/assets/no-media.png', alt: 'No media' }}
         />
       ) : (
