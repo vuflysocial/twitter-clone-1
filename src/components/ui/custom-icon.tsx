4277248d9b334b3222108c13b8b5febce38d54f1
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-04-10 11:52:54 -0700

    Update custom-icon.tsx

--- src/components/ui/custom-icon.tsx
+++ src/components/ui/custom-icon.tsx
@@ -84,12 +84,12 @@ function SpinnerIcon({ className }: IconProps): JSX.Element {
         cx='12'
         cy='12'
         r='10'
-        stroke='currentColor'
+        stroke='green'
         strokeWidth='4'
       />
       <path
         className='opacity-75'
-        fill='currentColor'
+        fill='green'
         d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
       />
     </svg>
@@ -191,3 +191,4 @@ function PinOffIcon({ className }: IconProps): JSX.Element {
     </svg>
   );
 }
+
